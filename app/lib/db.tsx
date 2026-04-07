68bf8f5b4c86f42fe87f12e9162183b2c8a00291
Ajay <ajaykaloniya@gmail.com>
2026-04-08 02:21:56 +0530

    first commit

---
+++ app/lib/db.tsx
@@ -0,0 +1,52 @@
+export const questions = [
+  {
+    "question": "Which planet is known as the Red Planet?",
+    "options": ["Venus", "Mars", "Jupiter", "Saturn"],
+    "answer": "Mars"
+  },
+  {
+    "question": "What is the chemical symbol for Gold?",
+    "options": ["Gd", "Go", "Ag", "Au"],
+    "answer": "Au"
+  },
+  {
+    "question": "Who painted the 'Mona Lisa'?",
+    "options": ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Claude Monet"],
+    "answer": "Leonardo da Vinci"
+  },
+  {
+    "question": "Which is the largest ocean on Earth?",
+    "options": ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+    "answer": "Pacific Ocean"
+  },
+  {
+    "question": "In what year did the Titanic sink?",
+    "options": ["1905", "1912", "1920", "1931"],
+    "answer": "1912"
+  },
+  {
+    "question": "What is the hardest natural substance on Earth?",
+    "options": ["Gold", "Iron", "Diamond", "Quartz"],
+    "answer": "Diamond"
+  },
+  {
+    "question": "Which country is home to the Kangaroo?",
+    "options": ["South Africa", "Australia", "Brazil", "India"],
+    "answer": "Australia"
+  },
+  {
+    "question": "What is the capital city of Japan?",
+    "options": ["Seoul", "Beijing", "Tokyo", "Bangkok"],
+    "answer": "Tokyo"
+  },
+  {
+    "question": "How many bones are in the adult human body?",
+    "options": ["186", "206", "216", "226"],
+    "answer": "206"
+  },
+  {
+    "question": "Which gas do plants primarily use for photosynthesis?",
+    "options": ["Oxygen", "Nitrogen", "Hydrogen", "Carbon Dioxide"],
+    "answer": "Carbon Dioxide"
+  }
+]
