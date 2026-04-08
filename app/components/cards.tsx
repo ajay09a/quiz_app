a369aa372b7c50422c2f25070f076d4fa8342ef0
Ajay <ajaykaloniya@gmail.com>
2026-04-08 19:03:40 +0530

    add cards and it's functionality

---
+++ app/components/cards.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+
+export default function Cards({ question}: any) {
+    return (
+        <div>
+            <h2>{question}</h2>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import Cards from "./components/cards";
+import { questions } from "./lib/db";
+
 export default function Home(){
+  const [index, setIndex] = useState(0);
+  const [feedback, setFeedback] = useState<string>("");
+
+  const nextQue =()=>{
+        if(index<questions.length-1){
+            setIndex(index+1);
+            setFeedback("");
+        }
+    }
+
+  const checkAnswer = (selectedOption: string) => {
+        if (selectedOption === questions[index].answer) {
+            setFeedback("Correct! 🎉");
+        } else {
+            setFeedback(`Wrong! The correct answer is: ${questions[index].answer}`);
+        }
+    };
+
   return (
     <div>
-      Hello World
+      <Cards
+      question = {questions[index].question}
+      />
+      {questions[index].options.map((option, index) => (
+                    <button key={index} onClick={() => checkAnswer(option)}>
+                        {option}
+                    </button>
+                ))}
+      <p>{feedback}</p>
+      <button onClick={()=>nextQue()}>Next</button>
     </div>
   )
 }
